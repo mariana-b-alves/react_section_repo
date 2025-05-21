f22055a7acf764fc6dbd8824799d9ed07e149f0a
Mariana Alves <m.bourdain.alves>
2025-05-21 13:55:56 +0100

    Corrected pt. 2

--- flag-25-react-main/4-context-redux/components/PropDrilling.jsx
+++ flag-25-react-main/4-context-redux/components/PropDrilling.jsx
@@ -1,31 +1,35 @@
+import { useState } from "react";
+
 
 export default function PropDrilling() {
-    
-const [nome, setNome] = useState('João');
+
+    const [nome, setNome] = useState('Joao');
 
   return (
     <section>
-      <h1>Component Prop Drilling</h1>
-      <Hello nome={nome} changeName={ () => setNome('Maria')}/>
+        <h1>Componente PropDrilling</h1>
+        <Hello nome={nome} changeName={ ()=> setNome('Maria')} />
     </section>
-  )
+  );
 }
 
-function Hello (props){
+
+function Hello(props){
     return (
         <section>
-          <h1>Component Hello</h1>
-          <Greetings nome={props.nome} changeName={props.changeName}/>
+            {/* <h1>Componente Hello</h1> */}
+            <Greetings nome={props.nome} changeName={props.changeName} />
         </section>
-      )
+    )
 }
 
-function Greetings (props){
+
+function Greetings(props){
     return (
         <section>
-          <h1>Component Greetings</h1>
-          <p>Recebido do Avô (Via Pai): {props.nome}</p>
-          <button onClick={props.changeName}>Mudar Nome</button>
+            <h1>Componente Greetings</h1>
+            <p>Recebido do Avô (via Pai): {props.nome}</p>
+            <button onClick={props.changeName}> mudar nome</button>
         </section>
-      )
+    )
 }
