fdd55b34bb912b079fa47aaa9b5b8f6e76cb69a8
Mariana Alves <m.bourdain.alves>
2025-05-21 13:54:57 +0100

    Corrected

--- flag-25-react-main/3-side-effects/src/App.jsx
+++ flag-25-react-main/3-side-effects/src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
-import SayHello from '../components/SayHello'
-import WindowSize from '../components/WindowSize'
-import UsingLocalStorage from '../components/UsingLocalStorage'
-import UserList from '../components/UserList'
+import SayHello from './components/SayHello'
+import UsingLocalStorage from './components/UsingLocalStorage';
+import WindowSize from './components/WindowSize';
+import UserList from './components/UserList';
 
 function App() {
+ 
+
   return (
     <>
-      <h1>UseEffect Hook</h1>
-      <hr />
-      <SayHello />
-      <hr />
-      <WindowSize />
-      <hr />
-      <UsingLocalStorage />
-      <hr />
-      <UserList />
+     <h1>UseEffect Hook</h1>
+     <hr />
+     <SayHello />
+     <hr />
+     <WindowSize />
+     <hr />
+     <UsingLocalStorage />
+     <hr />
+     <UserList />
     </>
   )
 }
